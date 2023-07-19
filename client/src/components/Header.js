34b6e6b84afbb815dc3b58119ace9770cead4041
Avichai Iluz <Avichai997@gmail.com>
2023-07-19 10:32:28 +0300

    change css

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -18,7 +18,7 @@ const Header = {
         <button type="submit"><i class="fa fa-search"></i></button>
       </form>        
       </div>
-      <div>
+      <div class="links">
       ${name ? `<a href="/#/profile">${name}</a>` : `<a href="/#/signin">Sign-In</a>`}    
         <a href="/#/fashion-news">Fashion-news</a>
         <a href="/#/cart">Cart</a>
