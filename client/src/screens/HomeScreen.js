8aaf1526a4aabab20984b28a04bab5ed36bac835
NadavChen <nadav2282@gmail.com>
2023-07-20 16:23:49 +0300

    small changes - still in progress not working yet

--- client/src/screens/HomeScreen.js
+++ client/src/screens/HomeScreen.js
@@ -11,39 +11,51 @@ const HomeScreen = {
       return `<div class="error">${products.error}</div>`;
     }
 
-    return `
-    <ul class="products">
-      ${products
-        .map(
-          (product) => `
-      <li>
-        <div class="product">
-          <a href="/#/product/${product._id}">
-            <img src="${API}${product.image}" alt="${product.name}" />
-          </a>
-        <div class="product-name">
-          <a href="/#/product/${product._id}">
-            ${product.name}
-          </a>
-        </div>
-        <div class="product-rating">
-          ${Rating.render({
-            value: product.rating,
-            text: `${product.numReviews} reviews`,
-          })}
-        </div>
-        <div class="product-brand">
-          ${product.brand}
-        </div>
-        <div class="product-price">
-          $${product.price}
-        </div>
-        </div>
-      </li>
-      `
-        )
-        .join('\n')}
+    // Add the map container div to the rendered HTML
+    const mapContainerHTML = `
+      <div id="map" style="height: 300px;"></div>
     `;
+
+    const productsListHTML = products
+      .map(
+        (product) => `
+          <li>
+            <div class="product">
+              <a href="/#/product/${product._id}">
+                <img src="${API}${product.image}" alt="${product.name}" />
+              </a>
+              <div class="product-name">
+                <a href="/#/product/${product._id}">
+                  ${product.name}
+                </a>
+              </div>
+              <div class="product-rating">
+                ${Rating.render({
+                  value: product.rating,
+                  text: `${product.numReviews} reviews`,
+                })}
+              </div>
+              <div class="product-brand">
+                ${product.brand}
+              </div>
+              <div class="product-price">
+                $${product.price}
+              </div>
+            </div>
+          </li>
+        `
+      )
+      .join('\n');
+
+    // Combine the map container and products list in the html variable
+    const html = `
+      <ul class="products">
+        ${productsListHTML}
+      </ul>
+      ${mapContainerHTML}
+    `;
+    return html;
   },
 };
+
 export default HomeScreen;
