0efd74773d51f74b633a7702314e98dbbf2d0471
NadavChen <nadav2282@gmail.com>
2023-07-26 10:56:39 +0300

    added canvas object (DONE)

--- client/src/screens/FashionNewsScreen.js
+++ client/src/screens/FashionNewsScreen.js
@@ -15,6 +15,7 @@ const FashionNewsScreen = {
       <video class="video-container" controls>
       <source src="./src/assets/video/movie.mp4" type="video/mp4">
     </video>
+    <img class="canvas-image" src="./src/assets/video/cavas-image.jpg"></img>
     </div>
     <div>
     <ul class="news">
